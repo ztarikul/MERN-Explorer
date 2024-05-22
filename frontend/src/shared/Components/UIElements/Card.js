f8641482358c536b3c0b1291a5b044ad9ff118da
ztarikul <ztarikulislam@gmail.com>
2024-05-22 23:28:27 +0600

    main header add

--- frontend/src/shared/Components/UIElements/Card.js
+++ frontend/src/shared/Components/UIElements/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 const Card = (props) => {
   console.log(props);
   return (
-    <div className={"card " + props.className} style={props.style}>
+    <div className={`card ${props.className}`} style={props.style}>
       {props.children}
     </div>
   );
--- frontend/src/users/Components/UserItem.js
+++ frontend/src/users/Components/UserItem.js
@@ -6,24 +6,24 @@ import Card from "../../shared/Components/UIElements/Card";
 
 const UserItem = (props) => {
   return (
-    <li>
-      <div className="user-item">
-        <Card className="user-item_content">
-          {/*  alternative way `/${props.id}/places` */}
-          <Link to={"/" + props.id + "/places"}>
-            <div className="user-item_image">
-              <Avatar image={props.image} alt={props.name} />
-            </div>
-            <div className="user-item_info">
-              <h2>{props.name}</h2>
-              <h3>
-                {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
-              </h3>
-            </div>
-          </Link>
-        </Card>
-      </div>
+
+    <li className="user-item">
+      <Card className="user-item_content">
+        {/*  alternative way `/${props.id}/places` */}
+        <Link to={"/" + props.id + "/places"}>
+          <div className="user-item_image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item_info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
+
   );
 };
 
