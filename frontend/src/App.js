d02103d74cb5f16cfc5d8d1e36517ed933e8563b
ztarikul <ztarikulislam@gmail.com>
2024-05-24 23:21:30 +0600

    Place: List, Items, UserPlaces add with css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 import Users from "./users/Pages/Users";
 import NewPlaces from "./places/Pages/NewPlaces";
 import MainNavigation from "./shared/Components/Navigation/MainNavigation";
+import UserPlaces from "./places/Pages/UserPlaces";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <main>
         <Routes>
           <Route exact path="/" element={<Users />} />
-          <Route exact path="places/new" element={<NewPlaces />} />
+          <Route exact path="/:userId/places" element={<UserPlaces />} />
+          <Route exact path="/places/new" element={<NewPlaces />} />
         </Routes>
       </main>
 
