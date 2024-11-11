d2887c231e5e3486ab30e3172b18c917ac0091e5
ztarikul <ztarikulislam@gmail.com>
2024-11-11 17:13:57 +0600

    input form create and validation add

--- frontend/src/places/Pages/NewPlaces.js
+++ frontend/src/places/Pages/NewPlaces.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Input from "../../shared/Components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import './NewPlaces.css';
 
 const NewPlaces = () => {
 
-    return (
-        <div>
-            <h2>New Places Works!</h2>
-        </div>
-    )
+    return <form className="place-form">
+        <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title"/>
+        <Input element="" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter description"/>
+    </form>
 }
 
 export default NewPlaces;
