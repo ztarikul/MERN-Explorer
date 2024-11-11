635327cd98f72b540215db5285e245dd919f843a
ztarikul <ztarikulislam@gmail.com>
2024-11-12 00:43:23 +0600

    Sharing Input Values & Adding Multiple Inputs

--- frontend/src/places/Pages/NewPlaces.js
+++ frontend/src/places/Pages/NewPlaces.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Input from "../../shared/Components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
-import './NewPlaces.css';
+import {
+  VALIDATOR_MIN,
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./NewPlaces.css";
 
 const NewPlaces = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
 
-    return <form className="place-form">
-        <Input element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title"/>
-        <Input element="" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter description"/>
+  return (
+    <form className="place-form">
+      <Input
+        id="text"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element=""
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description. (at least 5 characters)"
+        onInput={descriptionInputHandler}
+      />
     </form>
-}
+  );
+};
 
 export default NewPlaces;
